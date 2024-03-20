1f026ec7181f04d3d1e20c69035f07172d2316c7
Ram <ibarahnin1@gmail.com>
2024-03-20 19:45:16 +0500

    Style

--- scripts/game/BigMsg/PS_BigMsgBox.c
+++ scripts/game/BigMsg/PS_BigMsgBox.c
@@ -3,9 +3,11 @@
 
 class PS_BigMsgBox : SCR_ScriptedWidgetComponent
 {
-	
 	ImageWidget m_wMsgIcon;
 	TextWidget m_wMsgText;
+	
+	ImageWidget m_wFill;
+	ImageWidget m_wEmpty;
 	
 	float m_fFadeTimeMax = 0.5;
 	float m_fShowTimeMax = 0.5;
@@ -23,6 +25,8 @@ class PS_BigMsgBox : SCR_ScriptedWidgetComponent
 		super.HandlerAttached(w);
 		m_wMsgIcon = ImageWidget.Cast(w.FindAnyWidget("MsgIcon"));
 		m_wMsgText = TextWidget.Cast(w.FindAnyWidget("MsgText"));
+		m_wEmpty = ImageWidget.Cast(w.FindAnyWidget("Fill"));
+		m_wFill = ImageWidget.Cast(w.FindAnyWidget("Empty"));
 				
 		AudioSystem.PlaySound("{06C02AFB2CA882EB}Sounds/UI/Samples/Menu/UI_Task_Created.wav");
 		
@@ -46,6 +50,11 @@ class PS_BigMsgBox : SCR_ScriptedWidgetComponent
 		}
 		m_wRoot.SetOpacity(opacity);
 		
+		float percent = m_fFadeTime / (m_fWholeTime - m_fShowTimeMax);
+		if (percent < 0)
+			percent = 0;
+		HorizontalLayoutSlot.SetFillWeight(m_wEmpty, percent);
+		HorizontalLayoutSlot.SetFillWeight(m_wFill, 1.0 - percent);
 		
 		float time = GetGame().GetWorld().GetWorldTime();
 		float deltaTime = time - m_fLastUpdateTime;
@@ -58,5 +67,8 @@ class PS_BigMsgBox : SCR_ScriptedWidgetComponent
 	void SetText(string text)
 	{
 		m_wMsgText.SetText(text);
+		
+		m_fFadeTime  = text.Length() * 0.08 + 2.5;
+		m_fWholeTime = text.Length() * 0.08 + 2.5;
 	}
 }
